b3fb6cc99b6559a49f5d89eec6b0a5bd3ac9e7ce
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2022-01-29 12:48:46 -0500

    First endPoint uvicorn running in http://127.0.0.1:8000←

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from src.app.routers.item import item_router
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.include_router(item_router)
