f9651f9f0883b024536437eb49e4730f5da09de2
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2022-01-30 12:37:59 -0500

    Unit test format_class_names

--- internal/platform/scraper/request.py
+++ internal/platform/scraper/request.py
@@ -2,32 +2,34 @@ from bs4 import BeautifulSoup
 import requests
 
 from internal.platform.scraper.models.path import Path
+
 from internal.platform.scraper.models.response import Response
 
 CLASS = "class"
 PARSER = "html.parser"
 
 
-class Request:
-    def single_request(self, path: Path):
-        response = Response()
+def single_request(path: Path):
+    response = Response()
 
-        url = requests.get(Path.base_url)
-        soup = BeautifulSoup(url.content, PARSER)
+    url = requests.get(Path.base_url)
+    soup = BeautifulSoup(url.content, PARSER)
 
-        class_names_title = format_class_names(path.title_class_name)
-        response.title = soup.find(path.title_tag, {CLASS: class_names_title}).text
+    class_names_title = format_class_names(path.title_class_name)
+    response.title = soup.find(path.title_tag, {CLASS: class_names_title}).text
 
-        #TODO para el texto o contenido
-        #TODO hacer esa parte modular
-        return response
-
+    # TODO para el texto o contenido
+    # TODO hacer esa parte modular
+    return response
 
 
 def format_class_names(names: [str]):
     class_names = ""
+    first = True
     for name in names:
+        if not first:
+            class_names += " "
+        first = False
         class_names += name
-        class_names += " "
 
     return class_names
--- internal/platform/scraper/test_request.py
+++ internal/platform/scraper/test_request.py
@@ -1,8 +1,26 @@
 import pytest
 from internal.platform.scraper.request import format_class_names
 
-NAMES = ["product-name__name", "alkosto-title-color"]
-FORMATTED_NAMES = "product-name__name alkosto-title-color"
 
-def test_format_class_names_when_ok_should_return_text():
-    assert format_class_names(NAMES) == FORMATTED_NAMES
+@pytest.mark.parametrize(
+    "input_a, expected",
+    [
+        (["product-name__name", "alkosto-title-color"], "product-name__name alkosto-title-color"),
+        (["title", "h1_title", "title_color"], "title h1_title title_color"),
+        (["title"], "title"),
+    ]
+)
+def test_format_class_names_when_ok_should_return_true(input_a, expected):
+    assert format_class_names(input_a) == expected
+
+
+@pytest.mark.parametrize(
+    "input_a, expected",
+    [
+        (["product-name__name", " alkosto-title-color"], "product-name__name alkosto-title-color"),
+        (["title", "h1_title", " title_color"], "title h1_title title_color"),
+        (["title"], " title"),
+    ]
+)
+def test_format_class_names_when_extra_space_should_return_false(input_a, expected):
+    assert format_class_names(input_a) != expected
