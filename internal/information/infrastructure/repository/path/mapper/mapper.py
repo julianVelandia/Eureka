31b6c5cf8e3a2ba3f00e7d38cb0e37f216db03c7
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2022-02-04 21:39:21 -0500

    path model

---
+++ internal/information/infrastructure/repository/path/mapper/mapper.py
@@ -0,0 +1,9 @@
+from internal.information.infrastructure.repository.path.model.path_model import PathModel
+
+
+class Mapper:
+
+    def model_to_domain(self, model: PathModel):
+        #entity = Path
+        #return entity
+        pass
