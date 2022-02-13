777f6b79a97a2984fb1ab285d96c57d3662702a8
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2022-02-12 22:52:09 -0500

    platform json package

--- internal/information/infrastructure/getpath/config/process.py
+++ internal/information/infrastructure/getpath/config/process.py
@@ -1,19 +1,22 @@
-from internal.information.core.query.get_config import QueryConfig as QueryEntity
+from internal.information.core.entity.path import Path as PathEntity
+from internal.platform.json.models.query import QueryModel as QueryPlatformModel
+
 from internal.information.infrastructure.getpath.config.model.query import QueryModel
 
 
 class MapperInterface:
-    def model_to_domain(self, query_model: QueryModel) -> QueryEntity:
-        pass
-
-    def json_to_model(self, query_model: QueryModel, json_config) -> QueryEntity:
+    def model_to_platform_model(self, query_model: QueryModel) -> QueryPlatformModel:
         pass
 
 
+class jsonMapping:
+    def mapping_json_config_to_path(self, query_model: QueryModel) -> PathEntity:
+        pass
 
 
 class ProcessPathConfig:
     mapper: MapperInterface
 
-    def get(self, query_model: QueryModel) -> QueryEntity:
-        return MapperInterface.model_to_domain(query_model)
+    def get(self, query_model: QueryModel) -> PathEntity:
+        platform_model = MapperInterface.model_to_platform_model(query_model)
+        return jsonMapping.mapping_json_config_to_path(platform_model)
