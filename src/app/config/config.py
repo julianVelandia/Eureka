c03a1a28de5a9200f5af1bf4a546e0b76a55835c
Julian Camilo Velandia <jvelandiag@unal.edu.co>
2022-02-15 17:01:41 -0500

    Feature/v0.3.0 02 wire (#15)
    * initial scaffolding of wire develop
    
    * config app file
    
    * Wire first version

---
+++ src/app/config/config.py
@@ -0,0 +1,5 @@
+'''
+class Config:
+    scope: ConstantScope
+    envConfig: EnviromentScopeConfig
+'''
---
+++ src/app/dependence/wire.py
@@ -0,0 +1,9 @@
+from src.handler.handler import Handler
+
+
+class HandlerContainer:
+    GetInformationHandler: Handler
+
+def new_wire() -> HandlerContainer:
+    pass
+    #conf = GetConfig()
