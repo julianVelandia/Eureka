53b959bf6984a3401faae382785d6314da9989ed
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2022-03-05 11:35:30 -0500

    template for runnign information done

--- src/app/routers/information.py
+++ src/app/routers/information.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from src.handler.getinformationbyconfig.contract.request import Params
 from src.handler.getinformationbyconfig.handler import Handler as HandlerInformationByConfig
 
 information_router = APIRouter()
 
+templates = Jinja2Templates(directory="internal/platform/templates/startproject")
 
-@information_router.get("/information/config/{language}/{config_name}")
-async def information_by_config(language: str, config_name: str):
+
+@information_router.get("/information/config/{language}/{config_name}", response_class=HTMLResponse)
+async def information_by_config(request: Request, language: str, config_name: str):
     params = Params(language, config_name)
-
-    return HandlerInformationByConfig().handler(params)
+    handler_response = HandlerInformationByConfig().handler(params)
+    return templates.TemplateResponse("show_information.html", {"request": request, "response": handler_response})
